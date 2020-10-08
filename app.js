447d1fe170cc05e08401af0e03b62495aac42a65
Cadelle1 <1024670967@qq.com>
2020-10-08 14:33:54 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//app.js
+App({
+    onLaunch: function () {
+        this.globalData = {}
+
+        let that = this;
+        wx.getSystemInfo({
+            success(res) {
+                that.windowWidth = res.windowWidth;
+                that.windowHeight = res.windowHeight;
+            }
+        })
+    }
+})
